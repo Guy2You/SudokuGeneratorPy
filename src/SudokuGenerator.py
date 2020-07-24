70d1c318dff6a47186a15eb8879d7fb871c65247
Ben Roberts <benjamindaveroberts@gmail.com>
2020-07-24 16:39:23 +0100

    Adds method to generate a solved sudoku puzzle
    Method is very unlikely to actually produce a puzzle but it works in theory.
    Needs work.

--- src/SudokuGenerator.py
+++ src/SudokuGenerator.py
@@ -1,9 +1,14 @@
+import random
+
+
 class SudokuPuzzle:
 
 	def __init__(self):
 		self.side_length = 9
 		self.sub_side_length = 3
-		self._grid = [[0 for i in range(self.side_length)] for j in range(self.side_length)]
+		self._grid = []
+		self.__clear_grid()
+		# number set should never contain any false equivalent values or this program will fail
 		self.number_set = set(range(1, 10))
 
 	def get_row(self, row):
@@ -30,13 +35,33 @@ class SudokuPuzzle:
 			raise ValueError(f"{row}, {column} is not a valid position in a puzzle with side length {self.side_length}")
 		return self._grid[row][column]
 
-	def set_tile(self, row, column, value):
+	def __set_tile(self, row, column, value):
 		if value not in self.number_set:
 			raise ValueError(
 				f"{value} is not a valid value in this puzzle. Valid values are: {', '.join(str(x) for x in self.number_set)}")
 		if row >= self.side_length or column >= self.side_length:
 			raise ValueError(f"{row}, {column} is not a valid position in a puzzle with side length {self.side_length}")
 		self._grid[row][column] = value
+
+	def __set_subgrid_tile(self, sub_row, sub_col, row, column, value):
+		"""
+		Sets the value of a tile within a given subgrid.
+		
+		:param sub_row: The subgrid row.
+		:param sub_col: The subgrid column.
+		:param row: The row in the subgrid.
+		:param column: The column in the subgrid.
+		:return: None
+		"""
+		row_offset = row * self.sub_side_length
+		column_offset = column * self.sub_side_length
+		if row_offset >= self.side_length or column_offset >= self.side_length:
+			raise ValueError(
+				f"{row}, {column} is not a valid subgrid position in a puzzle with side length {self.side_length} and sub side length {self.sub_side_length}")
+		if sub_row >= self.sub_side_length or sub_col >= self.sub_side_length:
+			raise ValueError(
+				f"{sub_row}, {sub_col} is not a valid position in a subgrid with side length {self.sub_side_length}")
+		self.__set_tile(row_offset + row, column_offset + column, value)
 
 	def is_full(self):
 		"""
@@ -82,12 +107,81 @@ class SudokuPuzzle:
 			invalid = invalid or any([x not in self.number_set and x != 0 for x in self.get_column(i)])
 		return invalid
 
+	def __generate_puzzle(self):
+		"""
+		Generates a solvable sudoku puzzle. If a puzzle already exists it will be overwritten.
+
+		:return: None
+		"""
+		self.__clear_grid()
+
+	def __clear_grid(self):
+		"""
+		Clears the puzzle grid. This can be done in order to prepare for a new puzzle to be made.
+
+		:return: None
+		"""
+		# a clear grid should always consist of false equivalent values in order for this program to work
+		self._grid = [[0 for i in range(self.side_length)] for j in range(self.side_length)]
+
+	def generate_solved(self):
+		"""
+		Generates a full puzzle grid with a solved puzzle in it.
+
+		:return: None
+		"""
+		for i in self.number_set:
+			for sr in range(self.sub_side_length):
+				for sc in range(self.sub_side_length):
+
+					free_rows = set(range(self.sub_side_length))
+					free_columns = set(range(self.sub_side_length))
+
+					# update free_rows
+					for j in range(self.sub_side_length):
+						if i in self.get_row(sr * self.sub_side_length + j):
+							free_rows.remove(j)
+					if len(free_rows) == 0:
+						# note: this applies to the subgrid (i.e. a 3 tile number of rows in a 9x9 puzzle)
+						raise Exception(
+							f"{i} is not placeable in the puzzle because it conflicts with itself in every row")
+
+					# update free_columns
+					for j in range(self.sub_side_length):
+						if i in self.get_column(sc * self.sub_side_length + j):
+							free_columns.remove(j)
+					if len(free_columns) == 0:
+						# note: this applies to the subgrid (i.e. a 3 tile number of columns in a 9x9 puzzle)
+						raise Exception(
+							f"{i} is not placeable in the puzzle because it conflicts with itself in every column")
+
+					# make set of all positions in subgrid for i
+					possible_positions = {(r, c) for r in free_rows for c in free_columns}
+
+					# try and place i in grid
+					while len(possible_positions) > 0:
+						position = random.choice(tuple(possible_positions))
+						possible_positions.remove(position)
+						# this if statement will only work if the .get_tile method returns a false equivalent value for an empty space
+						if not self.get_tile(sr * self.sub_side_length + position[0],
+											 sc * self.sub_side_length + position[1]):
+							self.__set_tile(sr * self.sub_side_length + position[0],
+											sc * self.sub_side_length + position[1], i)
+							break
+						elif len(possible_positions) == 0:
+							raise Exception(
+								f"{i} was unable to be placed in the puzzle because it has no valid available position in the puzzle")
+
+
+def dirty(puzzle):
+	try:
+		puzzle.generate_solved()
+	except:
+		dirty(puzzle)
+
 
 def main():
 	puzzle = SudokuPuzzle()
-	for i in range(9):
-		for j in range(9):
-			puzzle.set_tile(i, j, j + 1)
 	for i in range(9):
 		print(puzzle.get_row(i))
 
@@ -95,6 +189,11 @@ def main():
 	print(f"{'is valid: ':<25}{str(puzzle.is_valid())}")
 	print(f"{'is complete: ':<25}{str(puzzle.is_complete())}")
 	print(f"{'contains invalid values: ':<25}{str(puzzle.contains_invalid_values())}")
+
+	puzzle.generate_solved()
+
+	for i in range(9):
+		print(puzzle.get_row(i))
 
 
 if __name__ == "__main__": main()
